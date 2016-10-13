8a2a2d0f26cb8f05969f36b100dca6260491abc6
csi0n <huaqing.chen@bioon.com>
2016-10-13 14:57:02 +0800

    迭代更新

--- app/src/main/java/com/csi0n/blog/core/string/Constants.java
+++ app/src/main/java/com/csi0n/blog/core/string/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
     public static final String Preference_Name = "PBlog";
     public static final String cacheFolder = "Cache";
     public static final String BaseUrl = "http://10.0.1.31/blog/public/index.php/api/blog/";
+    public static final String BaseImgUrl = "http://10.0.1.31/blog/storage/resource/";
     public static final int CODE_SUCCESS = 200;
 }
